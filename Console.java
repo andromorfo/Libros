b4fdba29fcb4d40df4c1cc9ef14eb59eaa86e002
andromorfo <adrian_po_11235@hotmail.com>
2013-09-26 21:19:56 -0600

    Documentada la clase Console
    Comentarios explicando la función de la clase Console

--- Console.java
+++ Console.java
@@ -3,13 +3,13 @@ import java.io.*;
 public class Console {
 	
 	static StringBuilder response = new StringBuilder("");
-	
+	//Imprime el texto recibido en la consola
 	public static void println(String text) {
 		
 		System.out.println(text);
 		
 	}
-	
+	//Lee lo introducido en la consola evitando el uso de un try/catch
 	public static String readLine() {
 		
 		BufferedReader bf = new
